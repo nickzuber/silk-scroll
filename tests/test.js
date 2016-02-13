081e2fa22756ae815db5569f36f07e2f4dc30427
nickzuber <nickzoober@gmail.com>
2016-02-13 17:32:26 -0500

    okay, NOW node friendly..?

--- tests/test.js
+++ tests/test.js
@@ -1,12 +1,14 @@
 
+//var SilkScroll = require('../bin/silk-scroll.min.js');
 var SilkScroll = require('../src/silk-scroll.js');
 var test = require('tape');
 
 test('This should not throw any errors', function(t){
 
   try{
+    console.log(SilkScroll);
     var res = SilkScroll.active();
-    console.log(res);
+    t.equal(res, -1, "SilkScroll should return -1 within a node env");
   }catch(e){
     t.fail("Something went wrong with silk-scroll.\n" + e.message);
   }
