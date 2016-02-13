10fcf48b2b79ebe6bd1e7a5ecc69da090ba48e08
nickzuber <nickzoober@gmail.com>
2016-02-13 15:44:10 -0500

    constructing formal file structure

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+
+var gulp = require('gulp');
+var uglify = require('gulp-uglify'); // minify build files
+var rename = require('gulp-rename'); // custom name for build files
+var header = require('gulp-header'); // custom comment header on build files
+
+// Set banner for production file
+var pkg = require('./package.json');
+var banner = ['/*!',
+  ' // silk-scroll v<%= pkg.version %> | <%= pkg.license %> ',
+  ' // Copyright (c) 2015 <%= pkg.author %>',
+  ' */',
+  ''].join('\n');
+
+// Contacat & compress javascript files
+gulp.task('dispatch', function(){
+    gulp.src(['src/*.js'])
+    .pipe(uglify())
+    .pipe(rename({
+        basename: 'silk-scroll',
+        extname: '.min.js'
+    }))
+    .pipe(header(banner, {pkg: pkg}))
+    .pipe(gulp.dest('bin'));
+});
+
+// Set default to dispatch
+gulp.task('default', ['dispatch']);
